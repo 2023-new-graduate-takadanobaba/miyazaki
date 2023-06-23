b09161c34bb77b99b9ab7f319805d6da7437a543
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-23 17:42:02 +0900

    Update buu.java

--- buu/src/mazin/buu.java
+++ buu/src/mazin/buu.java
@@ -2,27 +2,40 @@ package mazin;
 
 import java.util.Scanner;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class buu {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("ナメェを記入してください");
         String name = scanner.nextLine();
-        System.out.println("こんにちは、"+name+"さん。");
-        
+        System.out.println("こんにちは、" + name + "さん。");
+
         System.out.println("ウメェもんを入力してくれ！");
-        Scanner foods = new Scanner(System.in);
-        String line = foods.nextLine();
-        String[] tmpList = line.split(",",0);
-        //tmpListは食べ物のこと
-        //下のlistは数値型のlistの箱
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<tmpList.length;i++)list.add(Integer.parseInt(tmpList[i]));
-            for(int i=0;i<list.size();i++){
-                System.out.print(list.get(i));
-                
-            if(i != tmpList.length - 1)System.out.print(",");
+        Scanner sc = new Scanner(System.in);
+
+        String line = sc.nextLine();
+        List<String> ListFoods = Arrays.asList(line.split(","));
+        System.out.println(ListFoods);
+        System.out.println(ListFoods.size());
+            if (line.length() == 0) {
+            System.out.println();
+        } else if (ListFoods.size() == 1) {
+            System.out.println(name + "さんの好きな食べ物は" + ListFoods.get(0) + "ですね");
+        } else if (ListFoods.size() > 1) {
+            System.out.print(name + "さんの好きな食べ物は");
+            for (int i = 0; i < ListFoods.size(); i++) {
+                // System.out.println(ListFoods.get(i) + "と、");
+                int size = ListFoods.size();
+                int indexOfLast = size - 1;
+                if (i == indexOfLast) {
+                    System.out.print(ListFoods.get(i));
+                }else{
+                System.out.print(ListFoods.get(i) + "と、");
+                }
+            }
+            System.out.print("です");
         }
+
     }
 }
