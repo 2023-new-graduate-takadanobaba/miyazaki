de05b04c26a54addee99fec900adc4ebbe55e411
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-26 16:48:01 +0900

    Create VendingMachine2.java

---
+++ buu/src/mazin/test2/VendingMachine2.java
@@ -0,0 +1,14 @@
+package mazin.test2;
+
+public class VendingMachine2 {
+
+   
+    public boolean isButtonPushed() {
+        return this.isButtonPushed();
+    }
+
+    public Button getButton(String item) {
+        return  "コーラ";
+    }
+    
+}
